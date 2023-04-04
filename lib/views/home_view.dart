b9d500b95eaa0875426e5cd9731dfa0d794fbbe3
anasahmed01 <zainkhilji2323@gmail.com>
2023-04-05 02:31:29 +0500

    home_view

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plantify_in/views/widgetviews/glob_widget.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -10,6 +11,84 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        title: Padding(
+          padding: const EdgeInsets.only(right: 10.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Image(
+                image: AssetImage('lib/assets/images/Group 46 (1).png'),
+              ),
+              text(
+                  text: 'Plantify',
+                  fontSize: 26,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black),
+            ],
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 5),
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Image(
+                    image: AssetImage('lib/assets/images/Group 153.png'),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Image(
+                    image: AssetImage('lib/assets/images/Frame 97.png'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            SizedBox(
+              width: 500,
+              height: 200,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.asset('lib/assets/images/Group 73.png',
+                    height: 150,
+                    width: MediaQuery.of(context).size.width,
+                    fit: BoxFit.cover),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.search_rounded),
+                  hintText: 'Search',
+                  suffixIcon: IconButton(
+                    onPressed: () {},
+                    icon: const Image(
+                      image: AssetImage('lib/assets/images/Group 76.png'),
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
